3509ba45d91c6ece7ca866a62976742e7abe6059
tuitenthai <21004148@st.vlute.edu.vn>
2024-08-13 16:37:39 +0700

    update trang chủ

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,67 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import Feather from '@expo/vector-icons/Feather';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+      <Tabs
+          screenOptions={{
+            tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+            headerShown: false,
+          }}>
+          <Tabs.Screen
+            name="index"
+            options={{
+              title: 'Khám Phá',
+              tabBarIcon: ({ color, focused }) => (
+                <Feather name="disc" size={24} color={color} />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="thuvien"
+            options={{
+              title: 'Thư Viện',
+              tabBarIcon: ({ color, focused }) => (
+                <MaterialCommunityIcons name={focused ? 'music-box-multiple' : 'music-box-multiple-outline'} size={24} color={color} />
+              ),
+            }}
+          />
+
+          <Tabs.Screen
+            name="banxephang"
+            options={{
+              title: 'Ranking',
+              tabBarIcon: ({ color}) => (
+                <FontAwesome6 name="ranking-star" size={24} color={color}/>
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="radio"
+            options={{
+              title: 'Radio',
+              tabBarIcon: ({ color, focused }) => (
+                <Feather name="radio" size={24} color={color} />
+              ),
+            }}
+          />
+           <Tabs.Screen
+            name="canhan"
+            options={{
+              title: 'Cá nhân',
+              tabBarIcon: ({ color, focused }) => (
+                <MaterialCommunityIcons name={focused ? 'account-music' : 'account-music-outline'} size={24} color={color} />
+              ),
+            }}
+          />         
+        </Tabs>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,126 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React,{useState,useEffect} from 'react'
+import { View,Text,StyleSheet,SafeAreaView,StatusBar,Dimensions,Image, ImageSourcePropType, ScrollView} from 'react-native'
+import Feather from '@expo/vector-icons/Feather';
+import Entypo from '@expo/vector-icons/Entypo';
+import Carousel from 'react-native-reanimated-carousel';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const index = () => {
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+interface BannerItem {
+  sectionId: string;
+  banner:string;
+  cover:string;
+  encodeId:string;
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  const width = Dimensions.get('window').width;
+  const [Banner, setBanner] = useState<BannerItem[]>([])
+  useEffect(() => {
+    const getHome = async () => {
+      try {
+        const response = await fetch(
+          'https://backendbaocaoweb.vercel.app/api/home',
+        );
+        const json = await response.json();
+        setBanner(json.data.items[0].items)
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getHome()
+  }, [])
+  return (
+    <ScrollView>
+      <SafeAreaView style={{flex:1,marginTop:StatusBar.currentHeight}}>
+            <View>
+              <View style={{
+                flexDirection:'row',
+                justifyContent:"space-between",
+                gap:10,
+                padding:10,
+                alignItems:'center',
+              }}>
+                <View>
+                  <Text style={style.Title}>
+                    Khám Phá
+                  </Text>
+                </View>
+                <View style={style.congcu}>
+                  <Text><Feather name="mic" size={27} color="black" /></Text>
+                  <Text><Entypo name="magnifying-glass" size={29} color="black" /></Text>
+                </View>
+              </View>
+    
+    {/* banner */}
+              <View style={{marginTop:6}}>
+                <Carousel
+                    data={Banner}
+                    loop
+                    width={width}
+                    height={width / 2}
+                    autoPlay={true}
+                    scrollAnimationDuration={1000}
+                    mode='parallax'
+                    renderItem={({ index,item }) => (
+                            <View
+                                style={{
+                                  flex:1,
+                                    alignItems:'center',
+                                    justifyContent: 'center',
+                                }}
+                            >
+                                <Image style={style.tinyLogo} source={{uri: item.banner,}}/>
+                            </View>
+                    )}
+                  />
+              </View>
+      {/* gợi ý bài hát */}
+              <ScrollView horizontal={true} style={style.scrollView}>
+
+              </ScrollView>     
+            </View>
+          </SafeAreaView>
+    </ScrollView>
+    
+  )
+}
+const style = StyleSheet.create({ 
+  layoutheader:{
+    flexDirection:'row',
+    justifyContent:"space-between",
+  },
+  Title:{
+    fontSize:25,
+    fontWeight:'700',
+  },
+  congcu:{
+    alignItems:"center",
+    fontSize:30,
+    fontWeight:'700',
+    flexDirection:'row',
+    gap:20,
+  },
+  tinyLogo: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    borderRadius: 20, 
+    borderWidth: 1,
+    borderColor: '#ffffff', 
+  },
+  scrollView: {
+    marginTop: 50,
+  },
+  box: {
+    width: 100,
+    height: 100,
+    justifyContent: 'center',
     alignItems: 'center',
-    gap: 8,
+    marginHorizontal: 10,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  text: {
+    color: 'white',
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+})
+
+export default index
